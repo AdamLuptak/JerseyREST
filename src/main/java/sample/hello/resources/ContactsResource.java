5906bbd215676e3000c5c3c75604550535d90953
Adam.Luptak <adam.luptak@t-systems.sk>
2015-11-25 16:06:58 +0100

    secpnd comitt

--- src/main/java/sample/hello/resources/ContactsResource.java
+++ src/main/java/sample/hello/resources/ContactsResource.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.CookieParam;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -16,9 +17,12 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.NewCookie;
 import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
 import javax.ws.rs.core.UriInfo;
 
 import sample.hello.bean.Address;
@@ -37,6 +41,41 @@ public class ContactsResource {
 	private HttpServletRequest request1;
 
 	@GET
+	@Path("uriinfo")
+	public void test(@Context UriInfo uriDetails) {
+		System.out.println("ALL query parameters -- " + uriDetails.getQueryParameters().toString());
+		System.out.println("'id' query parameter -- " + uriDetails.getQueryParameters().get("id"));
+		System.out.println("Complete URI -- " + uriDetails.getRequestUri());
+	}
+
+	@GET
+	@Path("httpheaders")
+	public void test(@Context HttpHeaders headers, @CookieParam(value = "localLastVisited") String localLastVisited) {
+		System.out.println("ALL headers -- " + headers.getRequestHeaders().toString());
+		System.out.println("'Accept' header -- " + headers.getRequestHeader("Accept"));
+	///	System.out.println("'TestCookie' value -- " + headers.getCookies().get("JSESSIONID").getValue());
+		System.err.println(headers.getCookies());
+	}
+
+	@GET
+	@Path("/login")
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response login(@CookieParam(value = "sdf") String localLastVisited) {
+		System.out.println(localLastVisited);
+	    NewCookie cookie = new NewCookie("name", "123");
+	    return Response.ok("OK").cookie(cookie).build();
+	}
+	
+	@GET
+	@Path("securitycontext")
+	public void test(@Context SecurityContext secContext) {
+		System.out.println("Caller -- " + secContext.getUserPrincipal().getName());
+		System.out.println("Authentication Scheme -- " + secContext.getAuthenticationScheme());
+		System.out.println("Over HTTPS ? -- " + secContext.isSecure());
+		System.out.println("Belongs to 'admin' role? -- " + secContext.isUserInRole("admin"));
+	}
+
+	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public Product getContacts() {
 		List<Contact> contacts = new ArrayList<Contact>();
@@ -48,6 +87,18 @@ public class ContactsResource {
 			// Set the session attri;butes as you wish
 		}
 		return new Product("sdfs", 12d);
+	}
+	
+	@GET
+	@Path("/foo")
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response foo(@CookieParam("name") String value) {
+	    System.out.println(value);
+	    if (value == null) {
+	        return Response.serverError().entity("ERROR").build();
+	    } else {
+	        return Response.ok(value).build();
+	    }
 	}
 
 	@GET
